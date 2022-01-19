93c9fa631a229b1807e2fc8390a999a8f7e7b507
yuu <yuu@tutamail.com>
2022-01-18 22:49:54 -0300

    Fix groups attr set placement

--- user/cnix.nix
+++ user/cnix.nix
@@ -2,21 +2,21 @@
 
 {
 
-  groups = {
-    cnix = { };
-  };
-
   # Define an user account. Don't forget to set a password with ‘passwd’.
-  users.users.cnix = {
-    isNormalUser = true;
-    createHome = true;
-    shell = pkgs.zsh;
-    group = "cnix";
-    extraGroups = [
-      "users"
-      "wheel" # for sudo
-    ];
-    home = "/home/cnix";
+  users = {
+    users.cnix = {
+      isNormalUser = true;
+      createHome = true;
+      shell = pkgs.zsh;
+      group = "cnix";
+      extraGroups = [
+        "users"
+        "wheel" # for sudo
+      ];
+      home = "/home/cnix";
+    };
+
+    groups.cnix = { };
   };
 
 }
