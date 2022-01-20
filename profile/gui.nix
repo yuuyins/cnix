f3153cf99248cfe75d24cc8102433b942025594b
yuu <yuu@tutamail.com>
2022-01-20 00:12:35 -0300

    Add systemd user service for sway

--- profile/gui.nix
+++ profile/gui.nix
@@ -27,6 +27,27 @@
       };
     };
 
+    systemd.user.services = {
+      # https://github.com/swaywm/sway/wiki/Systemd-integration#running-sway-itself-as-a---user-service
+      sway = {
+        Unit = {
+          Description = "sway - SirCmpwn's Wayland window manager";
+          Documentation = [ "man:sway(5)" ];
+          BindsTo = "graphical-session.target";
+          Wants = "graphical-session-pre.target";
+          After = "graphical-session-pre.target";
+        };
+        Service = {
+          Type = "simple";
+          EnvironmentFile = "-%h/.config/sway/env";
+          ExecStart = "/usr/bin/sway";
+          Restart = "on-failure";
+          RestartSec = "1";
+          TimeoutStopSec = "10";
+        };
+      };
+    };
+
     programs = {
       alacritty = {
         enable = true;
