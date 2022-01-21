128ca651c0ab02dbd44d042e2792e1473296a6d9
yuu <yuu@tutamail.com>
2022-01-21 02:18:30 -0300

    Add binary cache

--- profile/nix.nix
+++ profile/nix.nix
@@ -19,6 +19,14 @@
       experimental-features = nix-command flakes
     '';
 
+    binaryCaches = [
+      "https://nix-community.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    requireSignedBinaryCaches = true;
+    ];
+
     # gc = {
     #   automatic = true;
     #   dates = "weekly";
