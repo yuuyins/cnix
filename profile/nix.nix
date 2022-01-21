f8e4fac6e184a11ac2e354df7f0620bf05197b31
yuu <yuu@tutamail.com>
2022-01-21 02:19:45 -0300

    Fix position

--- profile/nix.nix
+++ profile/nix.nix
@@ -24,8 +24,8 @@
     ];
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    requireSignedBinaryCaches = true;
     ];
+    requireSignedBinaryCaches = true;
 
     # gc = {
     #   automatic = true;
